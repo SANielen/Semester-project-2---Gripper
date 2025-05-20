bbccf540d9ef4c5456386e1c0455caf6d8645a79
SANielen <113352106+SANielen@users.noreply.github.com>
2025-05-20 11:14:22 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <pigpio.h>
+#include <modbus/modbus.h>
 #include <iostream>
 #include <chrono>
 #include <thread>
@@ -121,8 +122,33 @@ void closeDatabase()
     	sqlite3_close(gripperDatabase);
 }
 
+modbus_t *ctx;			//Initialize modbus module variables
+modbus_mapping_t *mb_mapping;	//Initialize modbus module mapping
+
 int main() // Main loop for the whole program
 {
+	//Modbus connection to the UR robot
+	modbus_t *ctx = modbus_new_tcp("192.168.0.100", 502);  // IP of your UR robot
+	if (ctx == nullptr) {
+	std::cerr << "Unable to allocate libmodbus context\n";
+	return -1;
+	}
+	
+    //Internal dev Code: If debugging is needed, change values to -> true
+    bool addDebugging = true;
+    
+	
+    //Modbus connection check
+    if (modbus_connect(ctx) == -1) {
+    std::cerr << "Connection failed: " << modbus_strerror(errno) << "\n";
+    modbus_free(ctx);
+    return -1;
+    }
+	
+    //Initialize modbus table values and registers
+    int rc;
+    uint16_t values[5];
+	
     //Opening the database
     openDatabase();
     
@@ -150,7 +176,7 @@ int main() // Main loop for the whole program
                 mainGripper.setForwardPinState(0);
                 mainGripper.setBackwardPinState(0);
 
-                std::cout << " Type 1 to close gripper \n Type 2 to open gripper \n Type 3 to get the current status \n " << std::endl;
+                std::cout << " Type 1 to close gripper \n Type 2 to open gripper \n Type 3 to get the current status \n Type 4 to start active listening to ModbusCommands \n Type 5 add debugging logs to Modbus \n" << std::endl;
                 std::cin >> userInput;  // Takes userinput and sets the next case/command to call
 
                 if (userInput == 1) {       // Calls command to close the gripper
@@ -161,7 +187,8 @@ int main() // Main loop for the whole program
 
                 } else if (userInput == 3) { 
                     currentCase = 4;
-
+		} else if (userInput == 4) {
+		    currentCase = 5;
                 } else {
                     std::cout << "Please enter a valid option.\n\n" << std::endl;   // If input does not match an option, this will catch it and not throw an error
                 }
@@ -200,6 +227,99 @@ int main() // Main loop for the whole program
                 
                 currentCase = 1;
                 break;
+		
+        	case 5: //Start active listening of modbus commands
+			while (true) {
+			        rc = modbus_read_registers(ctx, 130, 5, values); // only need register 130
+			        if (rc == -1) {
+			            std::cerr << "Read failed: " << modbus_strerror(errno) << "\n";
+			            break;
+			        }
+			        if(addDebugging){
+			            std::cout << "-------------------------------------"<< "\n";
+			            std::cout << "Register 130 = " << values[0] << "\n";
+			            std::cout << "Register 131 = " << values[1] << "\n";
+			            std::cout << "Register 132 = " << values[2] << "\n";
+			            std::cout << "Register 133 = " << values[3] << "\n";
+			            std::cout << "Register 134 = " << values[4] << "\n";
+			        }
+			        
+			        if ((values[1] == 1 && values[4] == 1) || (values[3] == 1 && values[4] == 1)) {
+			            rc = modbus_write_register(ctx, 131, 0); //Reset open gripper
+			            rc = modbus_write_register(ctx, 133, 0); //reset close gripper
+			            rc = modbus_write_register(ctx, 134, 0); //reset resetvalue
+			            
+			        }
+			        //Close Gripper code:
+			        if (values[2] == 1) {
+			            std::cout << "-------------------------------------"<< "\n";
+			            std::cout << "Massa tells us to close da grippar\n";
+			            if(addDebugging){
+			            std::cout << "Register 130 = " << values[0] << "\n";
+			            std::cout << "Register 131 = " << values[1] << "\n";
+			            std::cout << "Register 132 = " << values[2] << "\n";
+			            std::cout << "Register 133 = " << values[3] << "\n";
+			            std::cout << "Register 134 = " << values[4] << "\n";
+			            }
+			            // Simulate gripper operation
+			            //sleep(2);  // Gripper action...
+					mainGripper.closeGripper();
+			            // Acknowledge by setting register 133 = 1
+			            rc = modbus_write_register(ctx, 133, 1);
+			            if (rc == -1) {
+			                std::cerr << "Failed to write 1 to register 133: " << modbus_strerror(errno) << "\n";
+			            } else {
+			                std::cout << "-------------------------------------"<< "\n";
+			                std::cout << "Successfully Close for Massa and asks for more work\n";
+			            if(addDebugging){
+			                std::cout << "Register 130 = " << values[0] << "\n";
+			                std::cout << "Register 131 = " << values[1] << "\n";
+			                std::cout << "Register 132 = " << values[2] << "\n";
+			                std::cout << "Register 133 = " << values[3] << "\n";
+			                std::cout << "Register 134 = " << values[4] << "\n";
+			            }
+			            }
+			        }        
+			        //open gripper code
+			        if (values[0] == 1) {
+			            std::cout << "-------------------------------------"<< "\n";
+			            std::cout << "Massa tells us to open da grippar\n";
+			        if(addDebugging){
+			            std::cout << "Register 130 = " << values[0] << "\n";
+			            std::cout << "Register 131 = " << values[1] << "\n";
+			            std::cout << "Register 132 = " << values[2] << "\n";
+			            std::cout << "Register 133 = " << values[3] << "\n";
+			            std::cout << "Register 134 = " << values[4] << "\n";
+			        }
+			            // Simulate gripper operation
+			            //sleep(2);  // Gripper action...
+					mainGripper.openGripper();
+			            // Acknowledge by setting register 131 = 1
+			            rc = modbus_write_register(ctx, 131, 1);
+			            if (rc == -1) {
+			                std::cerr << "Failed to write 1 to register 131: " << modbus_strerror(errno) << "\n";
+			            } else {
+			                std::cout << "-------------------------------------"<< "\n";
+			                std::cout << "Successfully Open for Massa and asks for more work\n";
+			            if(addDebugging){
+			                std::cout << "Register 130 = " << values[0] << "\n";
+			                std::cout << "Register 131 = " << values[1] << "\n";
+			                std::cout << "Register 132 = " << values[2] << "\n";
+			                std::cout << "Register 133 = " << values[3] << "\n";
+			                std::cout << "Register 134 = " << values[4] << "\n";
+			            }
+			            }
+			        }
+			        sleep(2); // delay
+			    }
+
+		break;
+		case 5:	//Update debugging setting
+			!addDebugging;
+			modbus_set_debug(ctx, addDebugging);
+			std::cout << "Debugging is now: " << addDebugging; << "\n";
+		break;
+		
         }
     }
     //Closing the database
